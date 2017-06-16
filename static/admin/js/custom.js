24cff210890631075c361daad8a8271b1586d3ff
Nhan Hoang <siege250294@gmail.com>
2017-06-16 13:45:13 +0700

    src: auto generate slugs when editing category name and post title

--- static/admin/js/custom.js
+++ static/admin/js/custom.js
@@ -247,3 +247,13 @@ if (typeof NProgress != 'undefined') {
         NProgress.done();
     });
 }
+
+// Slugify
+function slugify(text) {
+  return text.toString().toLowerCase()
+    .replace(/\s+/g, '-')           // Replace spaces with -
+    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/^-+/, '')             // Trim - from start of text
+    .replace(/-+$/, '');            // Trim - from end of text
+}
