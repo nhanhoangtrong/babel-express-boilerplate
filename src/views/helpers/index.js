910db70b980e496db81f3ce4e6aa91c405939ac6
Nhan Hoang <siege250294@gmail.com>
2017-07-09 14:45:12 +0700

    views: better helpers registration and rename "equal" to "ifeq"

--- src/views/helpers/index.js
+++ src/views/helpers/index.js
@@ -1,11 +1,32 @@
-import millisecondsToDate from './millisecondsToDate'
+const helpers = {
+    millisecondsToDate(milliseconds, options) {
+        const date = new Date(milliseconds || 0)
+        return date.toLocaleString()
+        // return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
+    },
+    millisecondsToVNDate() {
+        const date = new Date(arguments.length > 1 ? arguments[0] : 0)
+        const sep = arguments.length === 3 ? arguments[1] : '-'
+        return `${date.getDate()}${sep}${date.getMonth() + 1}${sep}${date.getFullYear()}`
+    },
+    showYoutubeEmbed(youtubePath, options) {
+        try {
+            const code = youtubePath.split('=')[1].split('&')[0];
+            return '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' + code + '" frameborder="0" allowfullscreen></iframe>'
+        } catch (err) {
+            return youtubePath
+        }
+    },
+    inc(value, options) {
+        return parseInt(value) + 1
+    },
+    ifeq(valueOne, valueTwo, options) {
+        return valueOne === valueTwo ? options.fn(this) : options.inverse(this)
+    },
+}
 
 export default (hbs) => {
-    hbs.registerHelper('millisecondsToDate', millisecondsToDate)
-    hbs.registerHelper('inc', function(value, options) {
-        return parseInt(value) + 1
-    })
-    hbs.registerHelper('equal', function(valueOne, valueTwo, options) {
-        return valueOne === valueTwo ? options.fn(this) : options.inverse(this)
-    })
+    for (let key in helpers) {
+        hbs.registerHelper(key, helpers[key])
+    }
 }
--- src/views/helpers/millisecondsToDate.js
+++
@@ -1,5 +0,0 @@
-export default (milliseconds, options) => {
-    const date = new Date(milliseconds)
-    // return `${date.getHours()}:${date.getMinutes()} ${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`
-    return date.toLocaleString()
-}
